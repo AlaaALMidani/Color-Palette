20c7124be00a7d43832c9057f9762c861f70b01b
Alaa Al-Midani <alaaalmedane@gmail.com>
2024-11-02 13:48:29 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { ColorRectangle } from "./components/ColorRectangle";
+import { ColorPalette } from "./components/ColorPalette";
+function App() {
+  return (
+    <div className="App">
+      <div className="h-full w-full">
+        <ColorPalette />
+      </div>
+    </div>
+  );
+}
+
+export default App;
